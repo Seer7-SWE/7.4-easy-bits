080229d08ca2bdaa7509186e40ce746738125761
Seer7-SWE <seeralan006@gmail.com>
2025-06-14 20:43:05 +0300

    key and default re added to atoms.js

--- src/atoms.js
+++ src/atoms.js
@@ -33,9 +33,12 @@
 
 import { atom, selector } from 'recoil';
 
-export const networkAtom = atom({ /* ... */ });
-export const jobsAtom = atom({ /* ... */ });
-export const messagingAtom = atom({ /* ... */ });
+export const networkAtom = atom({ key: "networkAtom",
+    default: 102 });
+export const jobsAtom = atom({ key: "jobsAtom",
+    default: 0 });
+export const messagingAtom = atom({  key: "messagingAtom",
+    default: 0 });
 
 export const notificationsAtom = atom({
   key: 'notificationsAtom',
